c110271374d6dc9f3cbdb2c939495a3c754f2517
bol04 <bruno.oliveira@sky.uk>
2016-05-31 17:16:09 +0100

    Fixed errors on mysql.

--- Server/quizz/src/main/resources/data.sql
+++ Server/quizz/src/main/resources/data.sql
@@ -2,16 +2,14 @@ insert into Show(id, text) values (1, 'Game of Thrones - S6.E7 The Broken Man');
 
 insert into Quiz(id, show_id) values (1, 1);
 
-insert into Question(id, text, quiz_id) values (1, 'What is the name of Jon Snow\'s Direwolf?', 1);
--- insert into Question(id, text, quizId) values (2, 'Dummy Sample question 2', 1);
--- insert into Question(id, text, quizId) values (3, 'Dummy Sample question 3', 1);
+insert into Question(id, text, quiz_id) values (1, 'What is the name of Jon Snows Direwolf?', 1);
 
 insert into Answer(id, text, question_id, correct) values (1, 'Grey Wind', 1, 0);
 insert into Answer(id, text, question_id, correct) values (2, 'Graham', 1, 0);
 insert into Answer(id, text, question_id, correct) values (3, 'Ghost', 1, 1);
 insert into Answer(id, text, question_id, correct) values (4, 'Gargamel', 1, 0);
 
-insert into Question(id, text, quiz_id) values (2, 'Name the "other" Stark child: Robb, Brandon, Sansa, Arya. Jon Snow and…?', 1);
+insert into Question(id, text, quiz_id) values (2, 'Name the other Stark child: Robb, Brandon, Sansa, Arya. Jon Snow and...?', 1);
 
 insert into Answer(id, text, question_id, correct) values (5, 'Rickon', 2, 1);
 insert into Answer(id, text, question_id, correct) values (6, 'Dickon', 2, 0);
