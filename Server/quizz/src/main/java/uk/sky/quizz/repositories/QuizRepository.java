2f57f6265b6631f8f7692f242d3b6d1af0e83953
Marlene Marques <marlene.marques@sky.uk>
2016-05-31 13:51:50 +0100

    adding quiz repo

---
+++ Server/quizz/src/main/java/uk/sky/quizz/repositories/QuizRepository.java
@@ -0,0 +1,8 @@
+package uk.sky.quizz.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uk.sky.quizz.domain.Quiz;
+
+public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+}
