8cf95712ca658d15f2dd92b86ed265afd14d42ec
Marlene Marques <marlene.marques@sky.uk>
2016-05-31 14:27:09 +0100

    Updating quiz repo

--- Server/quizz/src/main/java/uk/sky/quizz/repositories/QuizRepository.java
+++ Server/quizz/src/main/java/uk/sky/quizz/repositories/QuizRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uk.sky.quizz.domain.Quiz;
 
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+
+	Quiz findOneByShowId(Integer showId);
 }
