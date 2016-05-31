1107751e7ed3118a5c309f3779d4ecbbd7266f98
Bruno Santos <bruno.santos@sky.uk>
2016-05-31 14:12:53 +0100

    new solutions

--- Server/quizz/src/main/java/uk/sky/quizz/domain/Quiz.java
+++ Server/quizz/src/main/java/uk/sky/quizz/domain/Quiz.java
@@ -5,12 +5,12 @@ import java.util.Collection;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -19,7 +19,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@MappedSuperclass
+@Entity
 @Table(catalog = "quiz", schema = "")
 @XmlRootElement
 @EqualsAndHashCode(of = "id")
--- Server/quizz/src/main/java/uk/sky/quizz/repositories/QuizRepository.java
+++ Server/quizz/src/main/java/uk/sky/quizz/repositories/QuizRepository.java
@@ -1,6 +1,5 @@
 package uk.sky.quizz.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.sky.quizz.domain.Quiz;
 
